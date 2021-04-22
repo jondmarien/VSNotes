00763149bc57b31c03f259f6ad30ac03dbc50561
Jon Marien <nuclearsmilz@gmail.com>
2021-04-22 18:10:23 -0400

    added new notesQuestion commans

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,19 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "vsnotes" is now active!');
 
-	console.log('Congratulations, your extension "vsnotes" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vsnotes.helloWorld", () => {
+      vscode.window.showInformationMessage("Hello from VSNotes!");
+    })
+  );
 
-	let disposable = vscode.commands.registerCommand('vsnotes.helloWorld', () => {
-
-		vscode.window.showInformationMessage('Hello World from VSNotes!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vsnotes.notesQuestion", () => {
+      vscode.window.showInformationMessage("Do you like to make notes?", "uhh..sure?", "not really.");
+    })
+  );
 }
 
 export function deactivate() {}
